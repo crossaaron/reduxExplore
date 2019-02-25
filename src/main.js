411c4b4cafd7aeddddc60e5f7c3a106978fa13a4
crossaaron <atcbcrossx@gmail.com>
2019-02-24 19:25:37 -0800

    added sample counter w/ node / NO react

--- src/main.js
+++ src/main.js
@@ -1,7 +1,20 @@
-import React from 'react'
-import ReactDOM from  'react-dom'
+const createStore = require('redux').createStore;
 
-import App from './Components/App'
-import './style.css'
+// creating a 'reducer' function
+const counter = (state = 0, action) => {
+    switch(action.type) {
+        case 'increment':
+            return state + 1;
+        case 'decrement':
+            return state - 1;
+        default:
+            return state
+    }
+};
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const store = createStore(counter);
+store.subscribe(() => console.log(store));
+
+store.dispatch({type: 'increment'});
+store.dispatch({type: 'increment'});
+store.dispatch({type: 'decrement'});
